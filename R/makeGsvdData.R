60792f92f9dc549758fb0b6713e7cbe72bae75db
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-06-12 11:32:51 -0400

    added tileWdith

--- R/makeGsvdData.R
+++ R/makeGsvdData.R
@@ -1,11 +1,11 @@
-makeGsvdData <- function() {
+makeGsvdData <- function(tileWidth = 30000) {
     bwDir <- system.file("extdata", package = "gsvdData", mustWork = TRUE)
     bwFiles <- list.files(bwDir, pattern = "\\.bw", full = TRUE)
     names(bwFiles) <- sub("\\.bw", "", basename(bwFiles))
     ## FIXME: Bug in GenomicFiles
     bwFileList <- lapply(bwFiles, BigWigFile)
     gr <- unlist(tileGenome(seqlengths = c("chr22" = 51304566),
-                            tilewidth = 10^7))
+                            tilewidth = tileWidth))
     colData <- DataFrame(sampleName = names(bwFiles))
     colData$Individual <- sapply(strsplit(colData$sampleName, "_"), "[", 1)
     colData$Histone <- sapply(strsplit(colData$sampleName, "_"), "[", 2)
